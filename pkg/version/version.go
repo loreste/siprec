d706dda9846989ff867c36ef02c91da1e39424af
Lance Oreste <Lanceoreste@gmail.com>
2026-02-15 21:47:09 -0500

    chore: Bump version to 1.0.1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of the SIPREC server
-const Version = "1.0.0"
+const Version = "1.0.1"
 
 // UserAgent returns the User-Agent string for HTTP requests
 func UserAgent() string {
