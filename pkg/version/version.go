5b8a3e8427b59462c33d0b060951aaea3a083b5a
Lance oreste <lanceoreste@gmail.com>
2026-05-07 17:28:02 -0400

    release: Bump version to v1.2.2

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of the SIPREC server
-const Version = "1.2.1"
+const Version = "1.2.2"
 
 // UserAgent returns the User-Agent string for HTTP requests
 func UserAgent() string {
